accca7198533b22a53601bf51d81fe5bc3585420
Antonio Collesei <antonio.collesei@gmail.com>
2024-02-14 16:52:08 +0100

    Blacklist option
    Added possibility to blacklist columns (genes) so that they are excluded from the analysis

--- allstar.sh
+++ allstar.sh
@@ -6,19 +6,23 @@ source bio_venv/bin/activate
 num_cores=1
 top_k_rules=5
 graph_path='PPI/FIsInGene_122921_with_annotations.txt'
+blacklist='blacklist.csv'
 max_rule_length=4
 alpha=0.05
 threshold_manhattan=0.01
 y_value=0
 
 # parse command-line arguments
-while getopts ":p:f:s:k:y:g:l:a:t:" opt; do
+while getopts ":p:f:b:s:k:y:g:l:a:t:" opt; do
   case $opt in
     p)
       num_cores=$OPTARG
       ;;
     f)
       file_path=$OPTARG
+      ;;
+    b)
+      blacklist=$OPTARG # file containing blacklisted elements: one word per line
       ;;
     s)
       split=$OPTARG
@@ -53,10 +57,33 @@ filename=$(basename "$file_path")
 # set output file name
 output_file_name="results/ALLSTAR_runs/ALLSTAR_${filename}_${timestamp}.txt"
 
+# clean up the input file according to blacklisted elements (i.e. genes, if any)
+if [ ! -s "$blacklist" ]
+then
+  cp "$file_path" final_input_data.csv # no blacklisted words, so no need to modify the input file
+else
+  # read blacklisted words into an array
+  mapfile -t blacklist_words < "$blacklist"
+
+  # modify awk so that it removes the columns containing the blacklisted words from the input file
+  awk_blacklist='{ print '
+  for word in "${blacklist_words[@]}"; do
+    awk_blacklist+='$'$(awk -v word="$word" -F, 'NR==1 {for(i=1; i<=NF; i++) {if ($i ~ word) print i}}' "$file_path")
+    awk_blacklist+=','
+  done
+  awk_blacklist=${awk_blacklist::-1}  # Remove the trailing comma
+  awk_blacklist+=' }'
+
+  # use the modified awk
+  awk -F, "$awk_blacklist" "$file_path" > final_input_data.csv
+fi
+
+input_file_name='final_input_data.csv'
+
 echo "Calculating. Raw results in $output_file_name"
 # run ALLSTAR in calculus mode with the specified number of cores
 mpiexec -n $num_cores python py/allstar.py \
-    -f $file_path \
+    -f $input_file_name \
     -s $split \
     -k $top_k_rules \
     -y $y_value \
